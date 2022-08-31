ec3dee62140fcdeddb04a4c8043fa5c1bd6ac276
maualice <alicemauroe@gmail.com>
2022-08-31 01:18:28 -0300

    adding middleware for 404 route not found

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const app = express();
 //routers
 
 //error handler
+const notFoundMiddleware = require('./middleware/not-found');
 
 app.use(express.json());
 //routes
 app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route </a>');
 });
+
+app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
 
