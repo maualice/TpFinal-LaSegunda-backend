5f6a76ead30ccfda136c2ead6afca687ba45d10f
maualice <alicemauroe@gmail.com>
2022-08-31 01:22:17 -0300

    adding and testing products route

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 //connect DB
 
 //routers
-
+const productsRouter = require('./routes/products');
 //error handler
 const notFoundMiddleware = require('./middleware/not-found');
 
@@ -13,8 +13,21 @@ app.get('/', (req, res) => {
   res.send('<h1>Store API</h1><a href="/api/v1/products">products route </a>');
 });
 
+app.use('/api/v1/products', productsRouter);
+
 app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    //connectDB
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
