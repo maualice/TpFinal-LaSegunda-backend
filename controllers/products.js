d555cf73cd0588906f1ca8112f1aab4d8db608d8
maualice <alicemauroe@gmail.com>
2022-08-31 01:27:59 -0300

    improving product controller using statusCode package

--- controllers/products.js
+++ controllers/products.js
@@ -1,21 +1,23 @@
+const { StatusCodes } = require('http-status-codes');
+
 const getAllProducts = async (req, res) => {
-  res.send('get all products');
+  res.status(StatusCodes.OK).json({ msg: 'get all products' });
 };
 
 const getProduct = async (req, res) => {
-  res.send('get product');
+  res.status(StatusCodes.OK).json({ msg: 'get product' });
 };
 
 const createProduct = async (req, res) => {
-  res.send('create product');
+  res.status(StatusCodes.CREATED).json({ msg: 'create product' });
 };
 
 const updateProduct = async (req, res) => {
-  res.send('update product');
+  res.status(StatusCodes.OK).json({ msg: 'update product' });
 };
 
 const deleteProduct = async (req, res) => {
-  res.send('delete product');
+  res.status(StatusCodes.OK).json({ msg: 'delete product' });
 };
 
 module.exports = {
