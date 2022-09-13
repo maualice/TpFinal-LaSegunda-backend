8c1e3b04ccd9cb011d4aa8ead744dd5a4920eee1
maualice <alicemauroe@gmail.com>
2022-09-13 16:09:04 -0300

    documenting with swagger

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+//Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -51,8 +56,9 @@ app.use(xss());
 
 //routes
 app.get('/', (req, res) => {
-  res.send('<h1>Store API</h1><a href="/api/v1/products">products route </a>');
+  res.send('<h1>API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use('/api/v1/products', productsRouter);
 app.use('/api/v1/auth', authRouter);
